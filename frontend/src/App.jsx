ed046f7ce82a4fb82fa72e5234bfa7bed251fe85
Talal Jaber <talal123542006@gmail.com>
2025-08-12 22:48:18 +0300

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,66 +20,45 @@ import ThankYouPage from './pages/ThankYouPage';
 
 // Error Boundary Component
 import ErrorBoundary from './components/ErrorBoundary';
-
 function App() {
   return (
     <ErrorBoundary>
       <CartProvider>
         <Router>
-          <div className="flex flex-col min-h-screen">
+          <ScrollToTop />
+          <div className="min-h-screen flex flex-col">
             <Header />
-            
-            
-            
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductsPage />} />
+                <Route path="/products/:slug" element={<ProductDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/thank-you" element={<ThankYouPage />} />
+              </Routes>
+            </main>
             <Footer />
-            
-            {/* Toast Notifications */}
-            <ToastContainer
-              position="top-right"
-              autoClose={3000}
-              hideProgressBar={false}
-              newestOnTop
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-              toastClassName="!bg-white !text-text !border-2 !border-border !rounded-xl !shadow-lg"
-              progressClassName="!bg-accent"
-            />
           </div>
-           <ScrollToTop /> 
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<ProductsPage />} />
-              <Route path="/products/:slug" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/checkout" element={<CheckoutPage />} />
-              <Route path="/thank-you" element={<ThankYouPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-        <ToastContainer 
-          position="bottom-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-     
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            toastClassName="!bg-white !text-text !border-2 !border-border !rounded-xl !shadow-lg"
+            progressClassName="!bg-accent"
+          />
         </Router>
       </CartProvider>
     </ErrorBoundary>
   );
 }
+
 
 export default App;
