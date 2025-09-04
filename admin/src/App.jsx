b826124715102b8165c1b36847f51873af91ac64
Talal Jaber <talal123542006@gmail.com>
2025-09-05 01:22:57 +0300

    last commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -140,7 +140,6 @@ function App() {
         </Route>
         <Route path="/login" element={<Navigate to="/" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="orders/:id/edit" element={<OrderEdit />} />
       </Routes>
 
       <Toaster
